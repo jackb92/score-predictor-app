e1f8b4ce96166a9fae0450d2d5f1a987949bb6b6
Jack Bentley <jack.bentley@ao.com>
2021-02-15 13:43:28 +0000

    added tests and functionality for point calculator

--- components/GameCard.js
+++ components/GameCard.js
@@ -121,20 +121,40 @@ function GameCard() {
 	);
 }
 
-export default GameCard;
 export function getPoints(
 	teamAPrediction,
 	teamBPrediction,
 	teamAActual,
 	teamBActual
 ) {
-	if (teamAActual === 0) {
+	const exactScorelinePredicted =
+		teamAPrediction === teamAActual && teamBPrediction === teamBActual;
+	const correctResultPredicted =
+		teamAPrediction !== teamAActual || teamBPrediction !== teamBActual;
+	const correctResultTeamAWin =
+		teamAPrediction > teamBPrediction && teamAActual > teamBActual;
+	const correctResultTeamBWin =
+		teamBPrediction > teamAPrediction && teamBActual > teamAActual;
+	const correctResultDraw =
+		teamAPrediction === teamBPrediction && teamAActual === teamBActual;
+
+	if (exactScorelinePredicted) {
 		return 5;
 	}
 
-	if (teamAActual === 2) {
-		return 3;
+	if (correctResultPredicted) {
+		if (correctResultTeamAWin) {
+			return 3;
+		}
+		if (correctResultTeamBWin) {
+			return 3;
+		}
+		if (correctResultDraw) {
+			return 3;
+		}
 	}
 
 	return 0;
 }
+
+export default GameCard;
